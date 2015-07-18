ec6dfb8d7650b4ca612ce199b1311bc05e38900d
Emily Chao <elchao96@live.com>
2015-07-17 22:24:50 -0700

    can send SMS now

--- linkedinhackday-lap/linkedinhackday-lap.js
+++ linkedinhackday-lap/linkedinhackday-lap.js
@@ -13,46 +13,45 @@ if (Meteor.isClient) {
     // test: function () {
     //   return Session.get('test');
     // }
-  });
-
-  Template.hello.events({
-  
-    'submit form': function(event){
-        event.preventDefault();
-        // code goes here
-        name = event.target.name.value;
-        phone = event.target.phone.value;
-        console.log("name",name);
-        console.log("phone",phone);
-    }
-  });
-
-  Template.list.helpers({
-    twilio: function() {
-      return Twilio("AC174ce99727c8471edfc0f5731b544423", "f9c45bf2f7e702d0782106bc9027521a");
-    },
     phonenumber: function() {
       return "+18594485127";
     }
   });
 
-  Template.list.events({
-    'click button': function() {
+  Template.hello.events({
+    'submit form': function(event){
+      event.preventDefault();
+      // code goes here
+      name = event.target.name.value;
+      phone = event.target.phone.value;
+      console.log("name",name);
+      console.log("phone",phone);
+      Meteor.call("sendSMS", name, phone);
     }
   });
-
 }
-
-Meteor.Router
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+      Meteor.methods({
+        sendSMS: function(name, phone, message) {
+          twilio = Twilio("AC7c71001f691d9f320b0e69116042ee34", "8842bd2d380a05ba6e8197212dff6683");
+          twilio.sendSms({
+              to: phone, // Any number Twilio can deliver to 
+              from: '+18594485127', // A number you bought from Twilio and can use for outbound communication 
+              body: 'Hello ' + name + '!' // body of the SMS message 
+            }, function(err, responseData) { //this function is executed when a response is received from Twilio 
+              if (!err) { // "err" is an error received during the request, if any 
+                // "responseData" is a JavaScript object containing data received from Twilio. 
+                // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript): 
+                // http://www.twilio.com/docs/api/rest/sending-sms#example-1 
+                console.log(responseData.from); // outputs "+14506667788" 
+                console.log(responseData.body); // outputs "word to your mother." 
+              }
+              console.log(err);
+          });
+        }
+      });
   });
-
-
-
-
 }
-
-

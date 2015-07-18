e9de77ac0ecda4cac3c7c82894346daddc1db20f
Emily Chao <elchao96@live.com>
2015-07-17 21:28:28 -0700

    added sms file just in case, added iron-router, started a template

--- linkedinhackday-lap/linkedinhackday-lap.js
+++ linkedinhackday-lap/linkedinhackday-lap.js
@@ -14,6 +14,20 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.list.helpers({
+    twilio: function() {
+      return Twilio("AC174ce99727c8471edfc0f5731b544423", "f9c45bf2f7e702d0782106bc9027521a");
+    },
+    phonenumber: function() {
+      return "+18594485127";
+    }
+  });
+
+  Template.list.events({
+    'click button': function() {
+    }
+  });
 }
 
 if (Meteor.isServer) {
